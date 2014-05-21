fdba40b4a5086595aa37aab7e272210350574030
Harrison Ainsworth <hxa7241+git@googlemail.com>
2014-05-21 15:50:36 +0100

    Updated for version 2: * Added check mode and replace mode. * Added invalid byte count message to filter mode. * Added general failure message. Including: * Added out-of-range invalidity check.

--- utf8filter.ml
+++ utf8filter.ml
@@ -1,8 +1,8 @@
 #!/usr/bin/env ocaml
 (*------------------------------------------------------------------------------
 
-   UTF-8 filter (OCaml 4.00)
-   Harrison Ainsworth / HXA7241 : 2013
+   UTF-8 Filter 2 (OCaml 4.00)
+   Harrison Ainsworth / HXA7241 : 2012-2014
 
    http://www.hxa.name/tools/
 
@@ -12,9 +12,11 @@
 
 
 
+(* general part ------------------------------------------------------------- *)
+
 module Utf8 :
 sig
-   type result = Char of string | EOF | Fail
+   type result = Char of string | Invalid of string | EOF | Fail
    val readChar : in_channel -> result
 end =
 
@@ -49,10 +51,13 @@ let invalidities bytes =
             (((int_of_char bytes.[0]) / 16) = 0x0F)
    and isNonchar2 = (len = 3) &&
       ((bytes.[0] = '\xEF') && (bytes.[1] = '\xB7') &&
-      (bytes.[2] >= '\x90') && (bytes.[2] <= '\xA7')) in
+      (bytes.[2] >= '\x90') && (bytes.[2] <= '\xA7'))
+   and isTooHigh = (len = 4) &&
+      ((bytes.[0] >= '\xF4') && (bytes.[1] >= '\x90') &&
+      (bytes.[2]  >= '\x80') && (bytes.[3] >= '\x80')) in
 
    (isMalformed, isOverlong, isSurrogate,
-    isNonchar1a || isNonchar1b || isNonchar2)
+    isNonchar1a || isNonchar1b || isNonchar2, isTooHigh)
 
 
 let isComplete bytes =
@@ -63,69 +68,47 @@ let isComplete bytes =
    ( len = 4)
 
 
-type result = Char of string | EOF | Fail
+type result = Char of string | Invalid of string | EOF | Fail
 
 
 (**
- * Read next valid UTF-8 char from open file.
- * (Discards invalid chars until (moving forward) a valid one is found.)
+ * Read next valid UTF-8 char or invalid bytes from open file.
  *
- * Invalid meaning: malformed, overlong, surrogate, non-char.
+ * Effectively partitions the file into valid and invalid parts: so
+ * if you just output the Char and Invalid strings again, interleaved
+ * in order, you get exactly the original file.
  *
+ * Invalid meaning: malformed, overlong, surrogate, non-char, out-of-range.
  * According to: RFC-3629 -- http://tools.ietf.org/html/rfc3629
  * and: Unicode 6.1 -- http://www.unicode.org/versions/Unicode6.1.0/
  *
  * @param fileIn (in(out)) open file to read from
- * @return valid UTF-8 char (string) | EOF | Fail
+ * @return valid UTF-8 Char (string) | Invalid (string) | EOF | Fail
  *)
 let readChar fileIn =
 
-   let append bytes = bytes ^ string_of_byte (input_byte fileIn) in
+   let rec read bytes =
+      (* read and inspect a byte *)
+      let bytes = bytes ^ string_of_byte (input_byte fileIn) in
+      let (i0, i1, i2, i3, i4) = invalidities bytes in
 
-   let check bytes =
-      let (i0, i1, i2, i3) = invalidities bytes in
-      if i0 || i1 || i2 || i3 then begin
-         (* reset read-char state *)
-         if i0 && (String.length bytes > 1) then
+      (* valid so far: recurse for more, or return char *)
+      if not (i0 || i1 || i2 || i3 || i4) then
+         if not (isComplete bytes) then read bytes else Char bytes
+
+      (* invalid: return bytes *)
+      else
+         let len = String.length bytes in
+         (* maybe step back a byte in the read-stream *)
+         if i0 && (len > 1) then begin
             seek_in fileIn (pos_in fileIn - 1) ;
-         "" end
-      else bytes in
-
-   let rec read bytes = 
-      let bytes = check (append bytes) in
-      if not (isComplete bytes) then read bytes else Char bytes in
+            Invalid (String.sub bytes 0 (len - 1)) end
+         else Invalid bytes in
 
    try read ""
    with
    | End_of_file -> EOF
    | Sys_error _ -> Fail
-
-
-(* (* or, imperatively: *)
-let readChar fileIn =
-
-   try
-      let bytes = ref "" in
-
-      (* until complete char *)
-      while not (isComplete !bytes) do
-         (* read and accumulate byte *)
-         bytes := !bytes ^ string_of_byte (input_byte fileIn) ;
-
-         (* invalid resets read-char state *)
-         let (i0, i1, i2, i3) = invalidities !bytes in
-         if i0 || i1 || i2 || i3 then begin
-            if i0 && (String.length !bytes > 1) then
-               seek_in fileIn (pos_in fileIn - 1) ;
-            bytes := ""
-         end
-      done ;
-
-      Char !bytes
-
-   with
-   | End_of_file -> EOF
-   | Sys_error _ -> Fail *)
 
 end ;;
 
@@ -133,34 +116,99 @@ end ;;
 
 (* entry point -------------------------------------------------------------- *)
 
+type mode = Check | Filter | Replace ;;
+
+(* standard Unicode 'replacement char' U+FFFD UTF-8:EFBFBD *)
+let replacementChar = "\xEF\xBF\xBD" in
+
+
 (* check if help message needed *)
 if ((Array.length Sys.argv) > 1) &&
    ((Sys.argv.(1) = "-?") || (Sys.argv.(1) = "--help")) then
 
-   print_endline "\n  \
-        UTF-8 filter (OCaml 4.00)\n  \
-        Harrison Ainsworth / HXA7241 : 2013-01-18\n  \
-        http://www.hxa.name/\n\
+   print_endline "\n\
+      \ \ UTF-8 Filter 2 (OCaml 4.00)\n\
+      \ \ Harrison Ainsworth / HXA7241 : 2014-05-18\n\
+      \ \ http://www.hxa.name\n\
       \n\
-      Reads stdin, and writes to stdout only the valid UTF-8 content.\n\
-      (Invalid meaning: malformed, overlong, surrogate, non-char --\n\
-      according to RFC-3629 and Unicode 6.1.)\n\
+      Reads stdin and checks, filters, or replaces its valid UTF-8 content.\n\
+      * Check: returns status 0 if OK, else (invalid) returns status 1 and\n\
+      \ \ writes to stderr a message.\n\
+      * Filter: writes to stdout only the valid UTF-8 content.\n\
+      * Replace: writes to stdout the content with invalid parts replaced\n\
+      \ \ with 'replacement char's (U+FFFD EFBFBD).\n\
       \n\
-      usage:\n  \
-        utf8filter.ml < inFile > outFile\n"
+      (Invalid UTF-8 meaning: malformed, overlong, surrogate, non-char,\n\
+      out-of-range -- according to RFC-3629 and Unicode 6.1.)\n\
+      \n\
+      Usage:\n  \
+        utf8check.ml [-(c|f|r)] < inFile [> outFile]\n\
+      -c -- check (default)\n\
+      -f -- filter\n\
+      -r -- replace\n"
 
 (* otherwise execute *)
 else begin
 
    set_binary_mode_in  stdin  true ;
    set_binary_mode_out stdout true ;
+   set_binary_mode_out stderr true ;
+
+   (* choose mode of operation *)
+   let mode:mode =
+      if (Array.length Sys.argv) > 1 then
+         let option1 = Sys.argv.(1) in
+         match option1 with
+         | "-c" -> Check
+         | "-f" -> Filter
+         | "-r" -> Replace
+         | _    ->
+            begin
+               prerr_endline ("*** Failed: unrecognized option: " ^ option1) ;
+               exit 1
+            end
+      else Check in
+
+   let invalidCount = ref 0 in
 
    (* loop through chars until EOF or failure *)
    while true do
+
       match Utf8.readChar stdin with
-      | Utf8.Char c -> print_string c
-      | Utf8.EOF    -> exit 0
-      | Utf8.Fail   -> exit 1
+      | Utf8.Char c ->
+         (match mode with
+         | Check   -> ()
+         | Filter
+         | Replace -> print_string c)
+
+      | Utf8.Invalid s ->
+         begin
+            let len = String.length s in
+            if !invalidCount < (max_int - len) then
+               invalidCount := !invalidCount + len ;
+            match mode with
+            | Check
+            | Filter  -> ()
+            | Replace -> print_string replacementChar
+         end
+
+      | Utf8.EOF ->
+         begin
+            if !invalidCount <> 0 then
+               Printf.eprintf "Invalid byte count: %s%u\n"
+                  (if !invalidCount < max_int then "" else "at least ")
+                  !invalidCount ;
+            exit (match mode with
+               | Check   -> if !invalidCount = 0 then 0 else 1
+               | Filter
+               | Replace -> 0)
+         end
+
+      | Utf8.Fail ->
+         begin
+            prerr_endline "*** Failed: General/system failure." ;
+            exit 1
+         end
    done
 
 end
@@ -179,7 +227,7 @@ end
 illegal code points
 -------------------
 
-### surrogates
+### surrogates ###
 
 high: U+D800 - U+DBFF
 low:  U+DC00 - U+DFFF
@@ -193,7 +241,7 @@ U+D800 = ED A0 80
 U+DFFF = ED BF BF
 
 
-### non-chars
+### non-chars ###
 
 1a and 1b:
 
@@ -227,5 +275,13 @@ U+FDD0 - U+FDEF
 11111101 11010000 - 11111101 11101111
 11101111 10110111 10010000 - 11101111 10110111 10101111
 EF B7 90 - EF B7 A7
+
+
+### too high ###
+
+all at or above:
+U-110000  F4 90 80 80
+
+are out of range
 
 *)
